a28de172c01cc2bc08963240e6f35ea11121bf9b
Vinit Bodhwani <vinitbodhwani123@gmail.com>
2016-11-05 22:05:58 +0530

    changed ps

--- config/connections.js
+++ config/connections.js
@@ -34,7 +34,7 @@ module.exports.connections = {
 
   mongodb: {
     adapter: 'sails-mongo',
-    url: "mongodb://vinit:vinit@ds137267.mlab.com:37267/allaboutvit"
+    url: "mongodb://b_hacker:vinit123@B@ds137267.mlab.com:37267/allaboutvit"
   }
   //mongodb://<dbuser>:<dbpassword>@ds137267.mlab.com:37267/allaboutvit
   /***************************************************************************
