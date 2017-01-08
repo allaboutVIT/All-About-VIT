f15d92be7c8c91f6b647f0e1b0a7749b69b74bd8
Vinit Bodhwani <vinitbodhwani123@gmail.com>
2017-01-08 22:35:48 +0530

    testing

--- config/connections.js
+++ config/connections.js
@@ -34,7 +34,7 @@ module.exports.connections = {
 
   mongodb: {
     adapter: 'sails-mongo',
-    url: "mongodb://user:vinit123B@ds127978.mlab.com:27978/heroku_9n4jc0lx"
+    url: "mongodb://temp:tempuser@ds127978.mlab.com:27978/heroku_9n4jc0lx"
       // "mongodb://aav:vinit123aav@ds137267.mlab.com:37267/allaboutvit"
   }
   //mongodb://<dbuser>:<dbpassword>@ds137267.mlab.com:37267/allaboutvit
